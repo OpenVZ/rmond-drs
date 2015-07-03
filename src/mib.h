476dd67190c2003873d7874df28f90665dd927cd
Dmitry Mishin <dim@virtuozzo.com>
2015-07-04 00:45:33 +0300

    Fixed path to SDK headers

--- src/mib.h
+++ src/mib.h
@@ -5,7 +5,7 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
-#include <parallels-virtualization-sdk/Parallels.h>
+#include <prlsdk/Parallels.h>
 
 typedef std::vector<oid> Oid_type;
 
